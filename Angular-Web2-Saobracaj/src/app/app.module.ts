3c658d33f8ec51b085cc9f290d6e68ab94758eaf
Danilo <dmilosevic@live.com>
2019-06-01 16:36:43 +0200

    zavrsen login form component

--- Angular-Web2-Saobracaj/src/app/app.module.ts
+++ Angular-Web2-Saobracaj/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RedVoznjeComponent } from './red-voznje/red-voznje.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    RedVoznjeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, LoginFormComponent, RedVoznjeComponent]
 })
 export class AppModule { }
---
+++ Angular-Web2-Saobracaj/src/app/red-voznje/red-voznje.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-red-voznje',
+  templateUrl: './red-voznje.component.html',
+  styleUrls: ['./red-voznje.component.css']
+})
+export class RedVoznjeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
